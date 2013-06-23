027f15519f6967622d018bf035be8cb7ae3f4cf1
Martin Keegan <martin@no.ucant.org>
2013-06-23 21:06:28 +0100

    add (c) boilerplate

--- lib/tcp_server.ml
+++ lib/tcp_server.ml
@@ -1,3 +1,13 @@
+
+(*
+  Tcp_server, a TCP server generator for OCaml Lwt
+
+  Copyright (C) 2013  Martin Keegan
+
+  This programme is free software; you may redistribute and/or modify
+  it under the terms of the Apache License v2.0
+*)
+
 open Unix
 open Lwt
 open Lwt_unix
